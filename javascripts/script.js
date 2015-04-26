abb2e01ff1e5fbaa61c7e135d038b41a0d39f46b
akosua kernizan <akernizan@gmail.com>
2015-04-26 18:03:40 -0400

    WIP result function, syntax error

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,80 +1,105 @@
-var $rockButton = $('#rock').val();
-var $paperButton = $('#paper').val();
-var $scissorsButton = $('#scissor').val();
+var $rockButton = $('#rock').text();
+var $paperButton = $('#paper').text();
+var $scissorsButton = $('#scissor').text();
 var $resetButton = $('#reset');
 var $resultsText = $('#results-text');
 
-var rpsArray = ['Rock', 'Paper', 'Scissors']
+// var rpsArray = ['Rock', 'Paper', 'Scissors']
 
-var userChoice;
-var computerChoice;
+var userChoice, computerChoice;
 
+function setComputerChoice() {
+    var rand = Math.random();
+    if (rand < 0.34) {
+        computerChoice = 'Rock';
+    } else {
+        if (rand <= 0.67) {
+          computerChoice = 'Paper';
+        } else {
+        computerChoice = 'Scissors';
+        }
+    }
+    $('#computer-text').text(computerChoice);
+};
 
-function setComputerChoice(){
-	computerChoice = Math.random();
-
-	if(computerChoice < 0.34){
-		computerChoice = 'Rock';
-	}else if (computerChoice <= 0.67){
-		computerChoice = 'Paper';
-	}else{
-		computerChoice = 'Scissors'
-	}
-	$('#computer-text').text(computerChoice);
-}
-
-var deactivateButton = function(currentButton) {
-	var buttonid = "#" + currentButton;
-	var buttons = ['#rock','#scissor','#paper'];
-	buttons.forEach(function(button) {
-		if(button != buttonid) {
-			$(button).css({'visibility': 'hidden'})
-		}
-	})
-}
-
-
-function showResult(){
-	if(userChoice == computerChoice ){
-		$resultsText.text("It's a tie!! ")
-	} else if{
-		
-	}
-
-}  
-
-$(function(){
-	$('#rock').on('click', function(){
-		$('#user-text').text('Rock');
-		setComputerChoice();
-		showResult();
-		deactivateButton(this.id);
-	})
-
-	$('#scissor').on('click', function(){
-		$('#user-text').text('Scissors');
-		setComputerChoice();
-		showResult();
-		deactivateButton(this.id);
-	})
-
-	$('#paper').on('click', function(){
-		$('#user-text').text('Paper');
-		setComputerChoice();
-		showResult();
-		deactivateButton(this.id);
-	})
-
-	$('#reset').on('click', function(){
-		$('#user-text').text('');
-		$('#computer-text').text('');
-		$('#results-text').text('');
-
-		$('#rock').css({'visibility': 'visible'});
-		$('#paper').css({'visibility': 'visible'});
-		$('#scissor').css({'visibility': 'visible'});
-
-	})
+var deactivateButton = function (currentButton) {
+    var buttonid = "#" + currentButton;
+    var buttons = ['#rock', '#scissor', '#paper'];
+    buttons.forEach(function (button) {
+        if (button != buttonid) {
+            $(button).css({
+                'visibility': 'hidden'
+            })
+        };
+    });
+};
 
 
-})
+function showResult(id) {
+    console.log(id, computerChoice)
+    userChoice = id.toUpperCase();
+    if (userChoice == computerChoice) {
+        $('#results-text').text('Tie');
+    };
+
+    if (new RegExp(id, "i").test("Paper")) {
+        if (computerChoice == 'Paper') {
+            $('#results-text').text('Paper Wins!!');
+        } else {
+          if (computerChoice == 'Scissors') {
+            $('#results-text').text('Rock Wins!!');
+        }
+        };
+    };
+
+    if (new RegExp(id, "i").test('Scissors')) {
+        if (computerChoice == 'Rock') {
+            $('#results-text').text('Rock Wins!!');
+        } else {
+            if (computerChoice == 'Paper') {
+                $('#results-text').text('Scissors Wins!!');
+            }
+        }
+    }
+
+} //end of function
+
+$(function () {
+    $('#rock').on('click', function () {
+        $('#user-text').text('Rock');
+        deactivateButton(this.id);
+        setComputerChoice();
+        showResult(this.id);
+    })
+
+    $('#scissor').on('click', function () {
+        $('#user-text').text('Scissors');
+        deactivateButton(this.id);
+        setComputerChoice();
+        showResult(this.id);
+    })
+
+    $('#paper').on('click', function () {
+        $('#user-text').text('Paper');
+        deactivateButton(this.id);
+        setComputerChoice();
+        showResult(this.id);
+    })
+
+    $('#reset').on('click', function () {
+        $('#user-text').text('');
+        $('#computer-text').text('');
+        $('#results-text').text('');
+
+        $('#rock').css({
+            'visibility': 'visible'
+        });
+        $('#paper').css({
+            'visibility': 'visible'
+        });
+        $('#scissor').css({
+            'visibility': 'visible'
+        });
+
+    })
+});
