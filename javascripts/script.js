1a13f60c1453f1c5b88d97c56fc2bd5100e27df7
akosua kernizan <akernizan@gmail.com>
2015-04-26 18:14:09 -0400

    fixed showResult function

--- javascripts/script.js
+++ javascripts/script.js
@@ -6,100 +6,103 @@ var $resultsText = $('#results-text');
 
 // var rpsArray = ['Rock', 'Paper', 'Scissors']
 
-var userChoice, computerChoice;
+var userChoice = 'Rock' || 'Paper' || 'Scissors';
+var computerChoice = 'Rock' || 'Paper' || 'Scissors';
 
-function setComputerChoice() {
-    var rand = Math.random();
-    if (rand < 0.34) {
+
+function setComputerChoice(){
+    computerChoice = Math.random();
+
+    if(computerChoice < 0.34){
         computerChoice = 'Rock';
-    } else {
-        if (rand <= 0.67) {
-          computerChoice = 'Paper';
-        } else {
+    }else if (computerChoice <= 0.67){
+        computerChoice = 'Paper';
+    }else{
         computerChoice = 'Scissors';
-        }
     }
     $('#computer-text').text(computerChoice);
 };
 
-var deactivateButton = function (currentButton) {
+var deactivateButton = function(currentButton) {
     var buttonid = "#" + currentButton;
-    var buttons = ['#rock', '#scissor', '#paper'];
-    buttons.forEach(function (button) {
-        if (button != buttonid) {
-            $(button).css({
-                'visibility': 'hidden'
-            })
+    var buttons = ['#rock','#scissor','#paper'];
+    buttons.forEach(function(button) {
+        if(button != buttonid) {
+            $(button).css({'visibility': 'hidden'})
         };
     });
 };
 
 
-function showResult(id) {
-    console.log(id, computerChoice)
-    userChoice = id.toUpperCase();
-    if (userChoice == computerChoice) {
+function showResult(){
+    console.log(userChoice, computerChoice);
+    if ( userChoice == computerChoice){
         $('#results-text').text('Tie');
-    };
+    }
+    else if (userChoice == 'Rock') {
 
-    if (new RegExp(id, "i").test("Paper")) {
         if (computerChoice == 'Paper') {
             $('#results-text').text('Paper Wins!!');
-        } else {
-          if (computerChoice == 'Scissors') {
+        } 
+        else if(computerChoice == 'Scissors') {
             $('#results-text').text('Rock Wins!!');
-        }
-        };
-    };
-
-    if (new RegExp(id, "i").test('Scissors')) {
-        if (computerChoice == 'Rock') {
-            $('#results-text').text('Rock Wins!!');
-        } else {
-            if (computerChoice == 'Paper') {
-                $('#results-text').text('Scissors Wins!!');
-            }
+            
         }
     }
+    else if(userChoice == 'Scissors'){
 
-} //end of function
+        if(computerChoice == 'Rock'){
+            $('#results-text').text('Rock Wins!!');
+        }
+        else if(computerChoice == 'Paper'){
+            $('#results-text').text('Scissors Wins!!');
+        }
+    }     
+    else if(userChoice == 'Paper'){
 
-$(function () {
-    $('#rock').on('click', function () {
+        if(computerChoice == 'Rock'){
+            $('#results-text').text('Paper Wins!!');
+        }
+        else if(computerChoice == 'Scissors'){
+            $('#results-text').text('Scissors Wins!!');
+        }
+    }  
+}//end of function
+$(function(){
+    $('#rock').on('click', function(){
         $('#user-text').text('Rock');
+        userChoice = 'Rock';
         deactivateButton(this.id);
         setComputerChoice();
-        showResult(this.id);
+        showResult();
     })
 
-    $('#scissor').on('click', function () {
+    $('#scissor').on('click', function(){
         $('#user-text').text('Scissors');
+        userChoice = 'Scissors';
         deactivateButton(this.id);
         setComputerChoice();
-        showResult(this.id);
+        showResult();
     })
 
-    $('#paper').on('click', function () {
+    $('#paper').on('click', function(){
         $('#user-text').text('Paper');
+        userChoice = 'Paper';
         deactivateButton(this.id);
         setComputerChoice();
-        showResult(this.id);
+        showResult();
     })
 
-    $('#reset').on('click', function () {
+    $('#reset').on('click', function(){
         $('#user-text').text('');
         $('#computer-text').text('');
         $('#results-text').text('');
 
-        $('#rock').css({
-            'visibility': 'visible'
-        });
-        $('#paper').css({
-            'visibility': 'visible'
-        });
-        $('#scissor').css({
-            'visibility': 'visible'
-        });
+        $('#rock').css({'visibility': 'visible'});
+        $('#paper').css({'visibility': 'visible'});
+        $('#scissor').css({'visibility': 'visible'});
 
     })
-});
+
+
+})
